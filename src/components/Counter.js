ed7a0405a1ca0991cd2f0d4cb0e6d91f8fd32e04
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-21 12:02:07 +0600

    connect api

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from './redux/counter/actions';
 
-const Counter = () => {
 
-    const [count, setCount] = useState(0);
+const Counter = ({ count, increment, decrement }) => {
 
-    const increment = () => {
-        setCount((prevCount) => prevCount + 1);
-    };
 
-    const decrement = () => {
-        setCount((prevCount) => prevCount - 1);
-    };
 
     return (
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
@@ -33,4 +28,18 @@ const Counter = () => {
     );
 };
 
-export default Counter;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        count: state.value,
+    };
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: (value) => dispatch(increment(value)),
+        decrement: (value) => dispatch(decrement(value)),
+    };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+
